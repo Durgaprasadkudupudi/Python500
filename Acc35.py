4b06ea7d64c4d225c9d2b0a42f9be8d7d8a2233e
Durgaprasadkudupudi <142087136+Durgaprasadkudupudi@users.noreply.github.com>
2025-02-09 22:20:54 +0530

    Add files via upload

---
+++ Acc35.py
@@ -0,0 +1,13 @@
+# cook your dish here
+t=int(input())
+for _ in range(t):
+    X,Y=map(int,input().split())
+    print(X-Y)
+
+# cook your dish here
+for _ in range(int(input())):
+    x=int(input())
+    if x>10:
+        print("NO")
+    else:
+        print("YES")
