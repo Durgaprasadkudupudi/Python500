7b05270c82f232680b8b2be445dec2ab21f93b6a
Durgaprasadkudupudi <142087136+Durgaprasadkudupudi@users.noreply.github.com>
2025-02-19 21:09:08 +0530

    Add files via upload

---
+++ Acc44.py
@@ -0,0 +1,20 @@
+# cook your dish here
+for _ in range(int(input())):
+    X=int(input())
+    if X>=80:
+        print("YES")
+    else:
+        print("NO")
+
+# cook your dish here
+for _ in range(int(input())):
+    x,y=map(int,input().split())
+    if y>x:
+        print("YES")
+    else:
+        print("NO")
+    
+    
+for _ in range(int(input())):
+    x=int(input())
+    print(x-10)
