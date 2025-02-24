4ef672314589512a3108308a37353236ad02fc73
Durgaprasadkudupudi <142087136+Durgaprasadkudupudi@users.noreply.github.com>
2025-02-24 21:53:44 +0530

    Add files via upload

--- Acc47.py
+++ Acc47.py
@@ -37,3 +37,33 @@ for _ in range(int(input())):
 for _ in range(int(input())):
     x,y=map(int,input().split())
     print(y//x)
+
+# cook your dish here
+for _ in range(int(input())):
+    x=int(input())
+    if x<3:
+        print("LIGHT")
+    elif x>=3 and x<7:
+        print("MODERATE")
+    else:
+        print("HEAVY")
+
+# cook your dish here
+for _ in range(int(input())):
+    a,b,c=map(int,input().split())
+    if a>b and a>c:
+        print("Alice")
+    elif b>a and b>c:
+        print("Bob")
+    else:
+        print("Charlie")
+
+# cook your dish here
+for _ in range(int(input())):
+    x=int(input())
+    if x<=70:
+        print(0)
+    elif x>70 and x<=100:
+        print(500)
+    else:
+        print(2000)
