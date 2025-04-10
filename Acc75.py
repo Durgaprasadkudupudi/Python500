0924b8e128025a264900cf1d8d4b19803f7a9655
Durgaprasadkudupudi <142087136+Durgaprasadkudupudi@users.noreply.github.com>
2025-04-10 21:17:14 +0530

    Add files via upload

---
+++ Acc75.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for _ in range(int(input())):
+    x=int(input())
+    if x<=100:
+        print(x)
+    elif 100<x<=1000:
+        print(x-25)
+    elif 1000<x<=5000:
+        print(x-100)
+    else:
+        print(x-500)
